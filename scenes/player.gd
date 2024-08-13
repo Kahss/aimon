5fb076bed377b7899ba5eb64477f60b2d60cfa94
Denis Maurel <d.maurel@groupeonepoint.com>
2024-08-13 14:58:25 +0200

    feat: collision

--- scenes/player.gd
+++ scenes/player.gd
@@ -21,8 +21,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	move(delta)
-	print(int(START_X - position.x) % delta_pos)
-	print(int(START_Y - position.y) % delta_pos)
+
 
 func move(delta):
 	if moving:
@@ -30,25 +29,36 @@ func move(delta):
 		
 	if (not moving or current_move_time == target_move_time):
 		if (Input.is_action_pressed("down")):
-			start_move("walking_down")
+			start_move("walking_down", Vector2i(0, 1))
 		elif (Input.is_action_pressed("up")):
-			start_move("walking_up")
+			start_move("walking_up", Vector2i(0, -1))
 		elif (Input.is_action_pressed("left")):
-			start_move("walking_left")
+			start_move("walking_left", Vector2i(-1, 0))
 		elif (Input.is_action_pressed("right")):
-			start_move("walking_right")
+			start_move("walking_right", Vector2i(1, 0))
 		else:
 			moving = false
 			stop()
+
+func blocked(delta: Vector2i):
+	var map: TileMap = get_parent().get_node("GroundTiles")
+	var charac_pos: Vector2i = map.local_to_map(Vector2(position.x, position.y))
+	var tile_data: TileData = map.get_cell_tile_data(0, charac_pos + delta)
+	return tile_data.get_collision_polygons_count(0) > 0
 			
-func start_move(move_str):
-	current_move_time = 0
-	moving = true
-	start_x = position.x
-	start_y = position.y
-	if (animation != move_str):
-		frame = 1
-	play(move_str)
+func start_move(move_str: String, delta: Vector2i):
+	if blocked(delta):
+		animation = move_str
+		pause()
+		moving = false
+	else:
+		current_move_time = 0
+		moving = true
+		start_x = position.x
+		start_y = position.y
+		if (animation != move_str):
+			frame = 1
+		play(move_str)
 		
 func update_position(delta):
 	var ratio = min(current_move_time + delta, target_move_time) / target_move_time
